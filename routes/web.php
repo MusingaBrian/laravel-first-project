8d168963e65cad17f4a3fc725a36320f6732b6b9
MusingaBrian <musingabrians@gmail.com>
2024-05-24 13:32:05 +0300

    Implement a Named Route

--- routes/web.php
+++ routes/web.php
@@ -1,30 +1,10 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ProductsController;
 
 Route::get('/', function () {
     return view('welcome');
 });
 
-//Route Return - String
-Route::get('/string', function () {
-    return 'Hello Laravel';
-});
-
-//Route Return - Array
-Route::get('/array', function () {
-    return ['HTML', 'CSS', 'PHP'];
-});
-
-//Route Return - JSON
-Route::get('/array', function () {
-    return response()->json([
-        'name' => 'Brian',
-        'Framework' => "Laravel"
-    ]);
-});
-
-//Route Return - Function
-Route::get('/function', function () {
-    return redirect('/');
-});
+Route::get('/products', [ProductsController::class, 'index'])->name('products');
