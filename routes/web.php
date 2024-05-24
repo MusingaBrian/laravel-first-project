d5ce9878eff9dfb70b504c82a619faffff8534a2
MusingaBrian <musingabrians@gmail.com>
2024-05-24 05:26:15 +0300

    Implement Basic Views And Routes

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ProductsController;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/products', [ProductsController::class, 'index']);
+Route::get('/products/about', [ProductsController::class, 'about']);
